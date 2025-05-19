c919ecf71d7b1c4939e28af29d0815b302e71ba3
Kyo1254 <60953739+Kyo1254@users.noreply.github.com>
2025-05-19 14:52:32 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char* argv)
+{
+    int b_year;
+    cin >> b_year;
+    // 불기 = 서기 + 543
+    
+    cout << b_year - 543;
+}
